84202f438beb44c2f4144a7b00cd5ee4ae769b23
JLamb <jlamb@harbrick.com>
2016-02-25 07:48:45 -0800

    added CAN writer C example.

---
+++ can_writer/src/can_writer.c
@@ -0,0 +1,499 @@
+/*
+ * Copyright (c) 2015 HARBRICK TECHNOLOGIES, INC
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * \example can_writer.c
+ *
+ * CAN API writer Example.
+ *
+ * Shows how to use the CAN API to write CAN frames.
+ *
+ * The example uses the standard PolySync node template and state machine.
+ * Send the SIGINT (control-C on the keyboard) signal to the node/process to do a graceful shutdown.
+ * See \ref polysync_node_template.h for more information.
+ *
+ */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// API headers
+#include "polysync_core.h"
+#include "polysync_node.h"
+#include "polysync_sdf.h"
+#include "polysync_message.h"
+#include "polysync_can.h"
+#include "polysync_node_template.h"
+
+
+
+
+// *****************************************************
+// static global types/macros
+// *****************************************************
+
+/**
+ * @brief Node flags to be OR'd with driver/interface flags.
+ *
+ * Provided by the compiler so Harbrick can add build-specifics as needed.
+ *
+ */
+#ifndef NODE_FLAGS_VALUE
+#define NODE_FLAGS_VALUE (0)
+#endif
+
+
+/**
+ * @brief CAN channel system index this example opens. [microseconds]
+ *
+ * Value 0 is first available channel.
+ *
+ */
+#define CAN_CHANNEL_SYSTEM_ID (0)
+
+
+/**
+ * @brief CAN bus bit rate this example uses.
+ *
+ */
+#define CAN_CHANNEL_BITRATE DATARATE_500K
+
+
+/**
+ * @brief PolySync node name.
+ *
+ */
+static const char NODE_NAME[] = "polysync-can-writer-c";
+
+
+
+
+// *****************************************************
+// static declarations
+// *****************************************************
+
+/**
+ * @brief Node template set configuration callback function.
+ *
+ * If the host provides command line arguments they will be set, and available
+ * for parsing (ie getopts).
+ *
+ * @note Returning a DTC other than DTC_NONE will cause the node to transition
+ * into the fatal state and terminate.
+ *
+ * @param [in] node_config A pointer to \ref ps_node_configuration_data which specifies the configuration.
+ *
+ * @return DTC code:
+ * \li \ref DTC_NONE (zero) if success.
+ *
+ */
+static int set_configuration(
+        ps_node_configuration_data * const node_config );
+
+
+/**
+ * @brief Node template on_init callback function.
+ *
+ * Called once after node transitions into the INIT state.
+ *
+ * @param [in] node_ref Node reference, provided by node template API.
+ * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
+ * @param [in] user_data A pointer to user data, provided by user during configuration.
+ *
+ */
+static void on_init(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data );
+
+
+/**
+ * @brief Node template on_release callback function.
+ *
+ * Called once on node exit.
+ *
+ * @param [in] node_ref Node reference, provided by node template API.
+ * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
+ * @param [in] user_data A pointer to user data, provided by user during configuration.
+ *
+ */
+static void on_release(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data );
+
+
+/**
+ * @brief Node template on_error callback function.
+ *
+ * Called continously while in ERROR state.
+ *
+ * @param [in] node_ref Node reference, provided by node template API.
+ * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
+ * @param [in] user_data A pointer to user data, provided by user during configuration.
+ *
+ */
+static void on_error(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data );
+
+
+/**
+ * @brief Node template on_fatal callback function.
+ *
+ * Called once after node transitions into the FATAL state before terminating.
+ *
+ * @param [in] node_ref Node reference, provided by node template API.
+ * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
+ * @param [in] user_data A pointer to user data, provided by user during configuration.
+ *
+ */
+static void on_fatal(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data );
+
+
+/**
+ * @brief Node template on_warn callback function.
+ *
+ * Called continously while in WARN state.
+ *
+ * @param [in] node_ref Node reference, provided by node template API.
+ * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
+ * @param [in] user_data A pointer to user data, provided by user during configuration.
+ *
+ */
+static void on_warn(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data );
+
+
+/**
+ * @brief Node template on_ok callback function.
+ *
+ * Called continously while in OK state.
+ *
+ * @param [in] node_ref Node reference, provided by node template API.
+ * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
+ * @param [in] user_data A pointer to user data, provided by user during configuration.
+ *
+ */
+static void on_ok(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data );
+
+
+
+
+// *****************************************************
+// static definitions
+// *****************************************************
+
+//
+static int set_configuration(
+        ps_node_configuration_data * const node_config )
+{
+    // local vars
+    ps_can_channel *can_channel = NULL;
+
+
+    // set node configuration default values
+
+    // node type
+    node_config->node_type = PSYNC_NODE_TYPE_API_USER;
+
+    // set node domain
+    node_config->domain_id = PSYNC_DEFAULT_DOMAIN;
+
+    // set node SDF key
+    node_config->sdf_key = PSYNC_SDF_ID_INVALID;
+
+    // set node flags
+    node_config->flags = NODE_FLAGS_VALUE | PSYNC_INIT_FLAG_STDOUT_LOGGING;
+
+    // set user data
+    node_config->user_data = NULL;
+
+    // set node name
+    memset( node_config->node_name, 0, sizeof(node_config->node_name) );
+    strncpy( node_config->node_name, NODE_NAME, sizeof(node_config->node_name) );
+
+    // create CAN channel
+    if( (can_channel = malloc( sizeof(*can_channel) )) == NULL )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- failed to allocate CAN channel data structure",
+                __FILE__,
+                __LINE__ );
+
+        return DTC_MEMERR;
+    }
+
+    // zero
+    memset( can_channel, 0, sizeof(*can_channel) );
+
+    // set user data pointer to our top-level node data
+    // this will get passed around to the various interface routines
+    node_config->user_data = (void*) can_channel;
+
+
+    return DTC_NONE;
+}
+
+
+//
+static void on_init(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data )
+{
+    // local vars
+    int ret = DTC_NONE;
+    ps_can_channel *can_channel = NULL;
+
+
+    // cast
+    can_channel = (ps_can_channel*) user_data;
+
+    // check reference since other routines don't
+    if( can_channel == NULL )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- invalid CAN channel",
+                __FILE__,
+                __LINE__ );
+
+        psync_node_activate_fault( node_ref, DTC_USAGE, NODE_STATE_FATAL );
+        return;
+    }
+
+    // open CAN channel
+    ret = psync_can_open(
+            can_channel,
+            CAN_CHANNEL_SYSTEM_ID,
+            PSYNC_CAN_OPEN_ALLOW_VIRTUAL );
+
+    // activate fatal error and return if failed
+    if( ret != DTC_NONE )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- psync_can_open returned DTC %d",
+                __FILE__,
+                __LINE__,
+                ret );
+
+        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
+        return;
+    }
+
+    // set bit rate
+    ret = psync_can_set_bit_rate(
+            can_channel,
+            CAN_CHANNEL_BITRATE );
+
+    // activate fatal error and return if failed
+    if( ret != DTC_NONE )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- psync_can_set_bit_rate returned DTC %d",
+                __FILE__,
+                __LINE__,
+                ret );
+
+        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
+        return;
+    }
+
+    // go on-bus
+    ret = psync_can_go_on_bus(
+            can_channel );
+
+    // activate fatal error and return if failed
+    if( ret != DTC_NONE )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- psync_can_go_on_bus returned DTC %d",
+                __FILE__,
+                __LINE__,
+                ret );
+
+        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
+        return;
+    }
+}
+
+
+//
+static void on_release(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data )
+{
+    // do nothing, sleep for 10 milliseconds
+    (void) psync_sleep_micro( 10000 );
+}
+
+
+//
+static void on_error(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data )
+{
+    // do nothing, sleep for 10 milliseconds
+    (void) psync_sleep_micro( 10000 );
+}
+
+
+//
+static void on_fatal(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data )
+{
+    // do nothing, sleep for 10 milliseconds
+    (void) psync_sleep_micro( 10000 );
+}
+
+
+//
+static void on_warn(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data )
+{
+    // do nothing, sleep for 10 milliseconds
+    (void) psync_sleep_micro( 10000 );
+}
+
+
+//
+static void on_ok(
+        ps_node_ref const node_ref,
+        const ps_diagnostic_state * const state,
+        void * const user_data )
+{
+    // local vars
+    int ret = DTC_NONE;
+    ps_can_frame can_frame;
+    ps_can_channel *can_channel = NULL;
+
+
+    // cast
+    can_channel = (ps_can_channel*) user_data;
+
+    // check reference since other routines don't
+    if( can_channel == NULL )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- invalid CAN channel",
+                __FILE__,
+                __LINE__ );
+
+        psync_node_activate_fault( node_ref, DTC_USAGE, NODE_STATE_FATAL );
+        return;
+    }
+
+    // zero
+    memset( &can_frame, 0, sizeof(can_frame) );
+
+    // set CAN frame data
+    can_frame.id = 0x456;
+    can_frame.dlc = 1;
+    can_frame.buffer[0] = 0xFF;
+
+    printf( "writing CAN frame - ID: 0x%lX (%lu) - DLC: %lu\n",
+                can_frame.id,
+                can_frame.id,
+                can_frame.dlc );
+
+    // write CAN frame
+    ret = psync_can_write(
+            can_channel,
+            &can_frame );
+
+    // activate fatal error and return if failed
+    if( ret != DTC_NONE )
+    {
+        psync_log_message(
+                LOG_LEVEL_ERROR,
+                "%s : (%u) -- psync_can_write returned DTC %d",
+                __FILE__,
+                __LINE__,
+                ret );
+
+        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
+        return;
+    }
+
+    // sleep for 100 milliseconds so we don't get a transmit buffer overflow
+    (void) psync_sleep_micro( 100000 );
+}
+
+
+
+
+// *****************************************************
+// public definitions
+// *****************************************************
+
+//
+int main( int argc, char **argv )
+{
+    // callback data
+    ps_node_callbacks callbacks;
+
+
+    // zero
+    memset( &callbacks, 0, sizeof(callbacks) );
+
+    // set callbacks
+    callbacks.set_config = &set_configuration;
+    callbacks.on_init = &on_init;
+    callbacks.on_release = &on_release;
+    callbacks.on_warn = &on_warn;
+    callbacks.on_error = &on_error;
+    callbacks.on_fatal = &on_fatal;
+    callbacks.on_ok = &on_ok;
+
+
+    // use PolySync main entry, this will give execution context to node template machine
+    return( psync_node_main_entry( &callbacks, argc, argv ) );
+}
